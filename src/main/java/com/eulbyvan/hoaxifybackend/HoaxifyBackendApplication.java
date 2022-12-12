e117cd3685c6f453a433eba2c99a3b25c1f015a8
eulbyvan <work.issdq@gmail.com>
2022-12-12 10:59:07 +0700

    Initial Commit

---
+++ src/main/java/com/eulbyvan/hoaxifybackend/HoaxifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eulbyvan.hoaxifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyBackendApplication.class, args);
+	}
+
+}
